4cb1a3941f947b6e3101588f03b2269f0b70c7c6
Fishheye <NeSkajy@haha.com>
2024-09-18 01:56:35 +0300

    kick

--- src/help_test.c
+++ src/help_test.c
@@ -4,10 +4,15 @@
 
 int main(void){
 
-
-    s21_decimal decimal = {0x800F0000, 0xFFFFFFFF, 0xFFFFFFCA, 0x2C962E45};
-    s21_decimal decimal2 = {0x000F0000, 0x0, 0x67, 0xD74625BB};
+    // -201180194235 | Обратный код сразу сделал
+    s21_decimal decimal = {0x800F0000, 0xFFFFFFFF, 0xFFFFFFD1, 0x28B9DA45};
+    
+    //  231180194235
+    s21_decimal decimal2 = {0x000F0000, 0x0, 0x35, 0xD369D1BB};
+    
+    // 30000000000 | 6fc23ac00
     s21_decimal result = {0x000F0000, 0x0, 0x0, 0x0};
+
     s21_decimal negate_result = {0x0, 0x0, 0x0, 0x0};
     // info_decimal(decimal);
     // s21_negate(decimal, &negate_result);
