c3ef950fb096fcb5bbb16474404ea6cc0b6b3717
Fishheye <NeSkajy@haha.com>
2024-09-20 19:14:03 +0300

    renamed s21_add to add_binary

--- src/s21_sub.c
+++ src/s21_sub.c
@@ -35,7 +35,7 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
         get_complement(value_2, &value_2n);
         set_sign_neg(&value_2n);
         
-        add_binary(value_1, value_2n, result);
+        s21_add(value_1, value_2n, result);
 
         if(get_sign(*result) == NEGATIVE) {
             get_complement(*result, result);
@@ -47,7 +47,7 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     */ 
     else if (value_1_sign == POSITIVE && value_2_sign == NEGATIVE) {
         s21_negate(value_2, &value_2);
-        add_binary(value_1, value_2, result);
+        s21_add(value_1, value_2, result);
     } 
     /* a - отрицательное, b - положитеьное => -a - b => -(a + b)
        Сложить a и b
@@ -57,7 +57,7 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
         s21_negate(value_1, &value_1);
         s21_negate(value_2, &value_2);
 
-        add_binary(value_1, value_2, result);
+        s21_add(value_1, value_2, result);
     } 
     /* a - отрицательное, b - отрицательное =>  -a - (-b) => -a + b
        Меньшее из a b число приводим к доп коду, далее a + b 
@@ -72,7 +72,7 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
         get_complement(value_1, &value_1n);
 
 
-        add_binary(value_2n, value_1n, result);
+        s21_add(value_2n, value_1n, result);
 
         if(get_sign(*result) == 1){
             get_complement(*result, result);
