b6e23e9f9697c3f6f4e4dc9aad896553f222aece
Fishheye <NeSkajy@haha.com>
2024-09-22 15:45:03 +0300

    init commit

--- src/s21_is_less.c
+++ src/s21_is_less.c
@@ -17,31 +17,6 @@ int bitwise_comparison(s21_decimal value_1, s21_decimal value_2) {
     return flag;
 }
 
-int s21_is_less_helper(s21_decimal value_1, s21_decimal value_2) {
-    int result = 0;
-    int value_sign_1 = get_sign(value_1);
-    int value_sign_2 = get_sign(value_2);
-    
-    if (value_sign_1 == NEGATIVE && value_sign_2 == POSITIVE) {
-        result = 2;
-    } else if (value_sign_1 == POSITIVE && value_sign_2 == NEGATIVE) {
-        result = 1;
-    } else if (value_sign_1 == POSITIVE && value_sign_2 == POSITIVE){
-        result = bitwise_comparison(value_1, value_2);
-    } else if (value_sign_1 == NEGATIVE && value_sign_2 == NEGATIVE) {
-        result = bitwise_comparison(value_1, value_2);
-        if (result == 1) {
-            result = 2;
-        } else if (result == 2) {
-            result = 1;
-        }
-    } else if (value_1.bits[0] == 0 && value_1.bits[1] == 0 && value_1.bits[2] == 0 && 
-    value_2.bits[0] == 0 && value_2.bits[1] == 0 && value_2.bits[2] == 0) {
-        result = 0;
-    }
-
-    return result;
-}
 
 int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
     int flag = 0;
