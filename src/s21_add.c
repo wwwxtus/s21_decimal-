478747f40a291282e6c5599dbe270c9cd102cfbd
Fishheye <NeSkajy@haha.com>
2024-10-09 01:05:58 +0300

    reworked division and s21_add

--- src/s21_add.c
+++ src/s21_add.c
@@ -16,7 +16,7 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
         int max_exp_v1 = max_exp(value_1);
         int max_exp_v2 = max_exp(value_2);
 
-        if(s21_is_greater(value_1, value_2)){
+        if(s21_is_greater(value_1, value_2) ){
             for(int i = 0; i <= max_exp_v1 - max_exp_v2; i++){
                 long_division(value_1, ten, &value_1);
                 original_exponent_v1 -= 1;
@@ -24,19 +24,14 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
             }
         }
 
-        // while(add_overflow_check(value_1, value_2, &temp_res)) {
-        //     if(s21_is_greater(value_1, value_2) && get_exponent(value_1) > 0){
+        if(s21_is_greater(value_2, value_1) ){
+            for(int i = 0; i <= max_exp_v2 - max_exp_v1; i++){
+                long_division(value_2, ten, &value_2);
+                original_exponent_v2 -= 1;
+                set_exponent(&value_2, original_exponent_v2);
+            }
+        }
 
-        //         long_division(value_1, ten, &value_1);
-        //         original_exponent_v1 -= 1;
-        //         set_exponent(&value_1, original_exponent_v1);
-
-
-        //     }else if(s21_is_greater(value_2, value_1) && get_exponent(value_2) > 0){
-        //         long_division(value_2, ten, &value_2);
-        //         set_exponent(&value_2, get_exponent(value_2) - 1);
-        //     }
-        // }
         level_exponent(&value_1, &value_2);
         add_binary(value_1, value_2, result);
     } else if (value_sign_1 == POSITIVE && value_sign_2 == NEGATIVE) {
@@ -48,6 +43,33 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     } else if (value_sign_1 == NEGATIVE && value_sign_2 == NEGATIVE) {
         set_sign_pos(&value_1);
         set_sign_pos(&value_2);
+
+        s21_decimal temp_res = {0, 0, 0, 0};
+        s21_decimal value_temp = {0, 0, 0, 0};
+        s21_decimal ten = {0xA, 0x0, 0x0, 0x0};
+
+        int original_exponent_v1 = get_exponent(value_1);
+        int original_exponent_v2 = get_exponent(value_2);
+
+        int max_exp_v1 = max_exp(value_1);
+        int max_exp_v2 = max_exp(value_2);
+
+        if(s21_is_greater(value_1, value_2) ){
+            for(int i = 0; i <= max_exp_v1 - max_exp_v2; i++){
+                long_division(value_1, ten, &value_1);
+                original_exponent_v1 -= 1;
+                set_exponent(&value_1, original_exponent_v1);
+            }
+        }
+
+        if(s21_is_greater(value_2, value_1) ){
+            for(int i = 0; i <= max_exp_v2 - max_exp_v1; i++){
+                long_division(value_2, ten, &value_2);
+                original_exponent_v2 -= 1;
+                set_exponent(&value_2, original_exponent_v2);
+            }
+        }
+
         add_binary(value_1, value_2, result);
         set_sign_neg(result);
     }
